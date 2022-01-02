452cd939e012fef8edad15d3076133706a130036
Randy Castleman <rcastleman@gmail.com>
2022-01-02 16:08:02 -0500

    deleted old commented lines

--- monte_carlo_python.py
+++ monte_carlo_python.py
@@ -82,6 +82,9 @@ hold_period_mode = 4.5
 hold_period_lower_bound = 1
 hold_period_upper_bound = 10
 
+model.range("C2").value = fund_size  #for convenience, for someone viewing the Excel sheet
+model.range("C3").value = num_companies #for convenience, for someone viewing the Excel sheet
+
 # Row of the Excel sheet where the first company's data goes
 COMPANIES_START_ROW = 16
 
@@ -112,148 +115,6 @@ def dcf_simulation():
         model.range(f"D{row}").value = company.capital_out
         model.range(f"E{row}").value = company.inv_date
         model.range(f"F{row}").value = company.exit_date
-
-#     model.range("C2").value = fund_size
-#     model.range("C3").value = num_companies
-
-    #capital in
-#     model.range("C16").value = -avg_initial_investment
-#     model.range("C17").value = -avg_initial_investment
-#     model.range("C18").value = -avg_initial_investment
-#     model.range("C19").value = -avg_initial_investment
-#     model.range("C20").value = -avg_initial_investment
-#     model.range("C21").value = -avg_initial_investment
-#     model.range("C22").value = -avg_initial_investment
-#     model.range("C23").value = -avg_initial_investment
-#     model.range("C24").value = -avg_initial_investment
-#     model.range("C25").value = -avg_initial_investment
-
-    #capital out
-#     company1_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D16").value = company1_capital_out
-#     company2_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D17").value = company2_capital_out
-#     company3_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D18").value = company3_capital_out
-#     company4_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D19").value = company4_capital_out
-#     company5_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D20").value = company5_capital_out
-#     company6_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D21").value = company6_capital_out
-#     company7_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D22").value = company7_capital_out
-#     company8_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D23").value = company8_capital_out
-#     company9_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D24").value = company9_capital_out
-#     company10_capital_out = avg_initial_investment * np.random.lognormal(returns_mean,returns_STD)
-#     model.range("D25").value = company10_capital_out
-    
-    #capital in date
-#     company1_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E16").value = company1_inv_date
-    
-#     company2_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E17").value = company2_inv_date
-    
-#     company3_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E18").value = company3_inv_date
-    
-#     company4_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E19").value = company4_inv_date
-    
-#     company5_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E20").value = company5_inv_date
-    
-#     company6_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E21").value = company6_inv_date
-    
-#     company7_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E22").value = company7_inv_date
-    
-#     company8_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E23").value = company8_inv_date
-    
-#     company9_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E24").value = company9_inv_date
-    
-#     company10_inv_date = inv_period_begin_date + timedelta(days = np.random.triangular(inv_period_lower_bound*days,
-#                                                          inv_period_mode*days,
-#                                                          inv_period_upper_bound*days))
-#     model.range("E25").value = company10_inv_date
-    
-    
-    
-
-#     #capital out date
-#     company1_exit_date = company1_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F16").value = company1_exit_date
-
-#     company2_exit_date = company2_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F17").value = company2_exit_date
-    
-#     company3_exit_date = company3_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F18").value = company3_exit_date
-    
-#     company4_exit_date = company3_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F19").value = company4_exit_date
-    
-#     company5_exit_date = company4_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F20").value = company5_exit_date
-    
-#     company6_exit_date = company1_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F21").value = company6_exit_date
-    
-#     company7_exit_date = company1_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F22").value = company7_exit_date
-    
-#     company8_exit_date = company1_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F23").value = company8_exit_date
-    
-#     company9_exit_date = company1_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F24").value = company9_exit_date
-    
-#     company10_exit_date = company1_inv_date + timedelta(days = np.random.triangular(hold_period_lower_bound*days,
-#                                                         hold_period_mode*days,
-#                                                         hold_period_upper_bound*days))
-#     model.range("F25").value = company10_exit_date
 
     #portfolio output
     portfolio_MOIC = model.range("C28").value
