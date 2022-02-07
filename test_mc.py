6e4008139b66bc735eeb60d8838d2f21ef2a653f
Randy Castleman <rcastleman@gmail.com>
2022-02-06 22:17:11 -0500

    modified tests

--- test_mc.py
+++ test_mc.py
@@ -1,6 +1,24 @@
-def test_sum():
-    assert sum([1, 2, 3]) == 6, "Should be 6"
+import unittest
 
-if __name__ == "__main__":
-    test_sum()
-    print("Everything passed")
+# def test_sum():
+#     assert sum([1, 2, 3]) == 6, "Should be 6"
+
+# def test_sum_tuple():
+#     assert sum((1, 2, 2)) == 6, "Should be 6"
+
+# if __name__ == "__main__":
+#     test_sum()
+#     test_sum_tuple()
+#     print("Everything passed")
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
