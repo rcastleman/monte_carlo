d16e1d0b2fc13a45c2af315d2120d8e3f6d2ea84
Randy Castleman <rcastleman@gmail.com>
2022-05-07 16:33:23 -0400

    defined inputs and outputs

--- simple_mc.py
+++ simple_mc.py
@@ -1,3 +1,4 @@
+import xlwings as xw
 import pandas as pd
 import random
 import numpy as np
@@ -6,3 +7,18 @@ import matplotlib.pyplot as plt
 from datetime import date,timedelta
 from pyxirr import xirr
 
+# connect workbook to program
+book = xw.Book('simple_mc.xlsx')  
+
+#define which sheet is the Model
+model = book.sheets('model')
+
+#write a value to the input cell in the Model
+model.range('A1').value = 8
+
+#define the input and output cells
+input = model.range('A1').value
+output = model.range('A2').value
+
+# print(book.sheets)
+print(f'the square of {input} is {output}.')
