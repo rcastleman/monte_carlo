a97bb9aaf17cce48c14c5a99db287ca749fbe821
Randy Castleman <rcastleman@gmail.com>
2022-05-07 16:04:49 -0400

    initial commit of a simple MC model

---
+++ simple_mc.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import random
+import numpy as np
+from numpy.random import uniform
+import matplotlib.pyplot as plt
+from datetime import date,timedelta
+from pyxirr import xirr
+
